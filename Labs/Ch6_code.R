86cba80ab258f2f111c25b41a62caf6e6486761a
zoenyoo <zoeyoo@berkeley.edu>
2021-10-30 15:35:22 -0400

    updates

--- Labs/Ch6_code.R
+++ Labs/Ch6_code.R
@@ -137,9 +137,114 @@ caret::confusionMatrix(testProbs$predOutcome, testProbs$Outcome,
                            positive = "1")
 
 
+# ROC Curve is useful because it visualizes trade-offs for two important confusion metrics
+# Also is a goodness-of-fit indicator
 ggplot(testProbs, aes(d = as.numeric(testProbs$Outcome), m = Probs)) +
   geom_roc(n.cuts = 50, labels = FALSE, colour = "#FE9900") +
   style_roc(theme = theme_grey) +
   geom_abline(slope = 1, intercept = 0, size = 1.5, color = 'grey') +
   labs(title = "ROC Curve - churnModel")
 
+# Area under the curve (AUC) measure: reasonable AUC is between 0.5 and 1
+pROC::auc(testProbs$Outcome, testProbs$Probs)
+
+# trainControl parameter is set to run 100 k-folds and to output predicted probabilities, classProbs, for ‘two classes’, Churn and No_Churn
+ctrl <- trainControl(method = "cv", number = 100, classProbs=TRUE, summaryFunction=twoClassSummary)
+
+# metrics in the cvFit output are for mean AUC, Sensitivity, and Specificity across all 100 folds.
+cvFit <- train(Churn ~ ., data = churn %>% 
+                 dplyr::select(
+                   -SeniorCitizen, 
+                   -WeekendBouncer,
+                   -churnNumeric), 
+               method="glm", family="binomial",
+               metric="ROC", trControl = ctrl)
+
+cvFit
+
+# plots the distribution of AUC, Sensitivity, and Specificity across the 100 folds
+# The tighter each distribution is to its mean, the more generalizable the model
+dplyr::select(cvFit$resample, -Resample) %>%
+  gather(metric, value) %>%
+  left_join(gather(cvFit$results[2:4], metric, mean)) %>%
+  ggplot(aes(value)) + 
+  geom_histogram(bins=35, fill = "#FF006A") +
+  facet_wrap(~metric) +
+  geom_vline(aes(xintercept = mean), colour = "#981FAC", linetype = 3, size = 1.5) +
+  scale_x_continuous(limits = c(0, 1)) +
+  labs(x="Goodness of Fit", y="Count", title="CV Goodness of Fit Metrics",
+       subtitle = "Across-fold mean reprented as dotted lines") +
+  plotTheme()
+
+# To calculate the total cost/benefit, these confusion metrics are multiplied by their corresponding costs (calculated in book)
+cost_benefit_table <-
+  testProbs %>%
+  count(predOutcome, Outcome) %>%
+  summarize(True_Negative = sum(n[predOutcome==0 & Outcome==0]),
+            True_Positive = sum(n[predOutcome==1 & Outcome==1]),
+            False_Negative = sum(n[predOutcome==0 & Outcome==1]),
+            False_Positive = sum(n[predOutcome==1 & Outcome==0])) %>%
+  gather(Variable, Count) %>%
+  mutate(Revenue =
+           case_when(Variable == "True_Negative"  ~ Count * 30,
+                     Variable == "True_Positive"  ~ ((30 - 8) * (Count * .50)) + 
+                       (-32 * (Count * .50)),
+                     Variable == "False_Negative" ~ (-30) * Count,
+                     Variable == "False_Positive" ~ (30 - 8) * Count)) %>%
+  bind_cols(data.frame(Description = c(
+    "We predicted no churn and did not send a mailer",
+    "We predicted churn and sent the mailer",
+    "We predicted no churn and the customer churned",
+    "We predicted churn and the customer did not churn")))
+
+# iterateThresholds iteratively loops through each threshold & calculates confusion metrics
+whichThreshold <- 
+  iterateThresholds(
+    data=testProbs, observedClass = Outcome, predictedProbs = Probs)
+
+whichThreshold[1:5,]
+
+# Next, the result is moved to long form and Revenue is calculated for each confusion metric at each threshold
+whichThreshold <- 
+  whichThreshold %>%
+  dplyr::select(starts_with("Count"), Threshold) %>%
+  gather(Variable, Count, -Threshold) %>%
+  mutate(Revenue =
+           case_when(Variable == "Count_TN"  ~ Count * 30,
+                     Variable == "Count_TP"  ~ ((30 - 8) * (Count * .50)) +
+                       (-32 * (Count * .50)),
+                     Variable == "Count_FN"  ~ (-30) * Count,
+                     Variable == "Count_FP"  ~ (30 - 8) * Count))
+
+# plots the Revenue for each confusion metric by threshold
+whichThreshold %>%
+  ggplot(.,aes(Threshold, Revenue, colour = Variable)) +
+  geom_point() +
+  scale_colour_manual(values = palette5[c(5, 1:3)]) +    
+  labs(title = "Revenue by confusion matrix type and threshold",
+       y = "Revenue") +
+  plotTheme() +
+  guides(colour=guide_legend(title = "Confusion Matrix")) 
+
+# calculate the total Revenue across confusion metrics for each threshold
+whichThreshold_revenue <- 
+  whichThreshold %>% 
+  mutate(actualChurn = ifelse(Variable == "Count_TP", (Count * .5),
+                              ifelse(Variable == "Count_FN", Count, 0))) %>% 
+  group_by(Threshold) %>% 
+  summarize(Revenue = sum(Revenue),
+            Actual_Churn_Rate = sum(actualChurn) / sum(Count),
+            Actual_Churn_Revenue_Loss =  sum(actualChurn * 30),
+            Revenue_Next_Period = Revenue - Actual_Churn_Revenue_Loss) 
+
+# Revenue (this period) and Revenue_Next_Period are plotted below for each Threshold
+whichThreshold_revenue %>%
+  dplyr::select(Threshold, Revenue, Revenue_Next_Period) %>%
+  gather(Variable, Value, -Threshold) %>%
+  ggplot(aes(Threshold, Value, colour = Variable)) +
+  geom_point() +
+  geom_vline(xintercept = pull(arrange(whichThreshold_revenue, -Revenue)[1,1])) +
+  scale_colour_manual(values = palette2) +
+  plotTheme() + ylim(0,70000) +
+  labs(title = "Revenue this pay period and the next by threshold",
+       subtitle = "Assuming no new customers added next period. Vertical line denotes optimal threshold")
